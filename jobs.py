208b786ba2cc0b53ece3214b8a18bc42e4b38181
King-Frosh <164648516+King-Frosh@users.noreply.github.com>
2026-08-14 11:44:09 +0100

    Add background job manager

---
+++ jobs.py
@@ -0,0 +1,40 @@
+import threading
+import uuid
+
+_jobs = {}
+_lock = threading.Lock()
+
+
+def create_job():
+    """Create a new background job and return its ID."""
+    job_id = uuid.uuid4().hex
+
+    with _lock:
+        _jobs[job_id] = {
+            "id": job_id,
+            "status": "queued",
+            "progress": 0,
+            "message": "Waiting to start",
+            "result": None,
+            "error": None,
+        }
+
+    return job_id
+
+
+def update_job(job_id, **kwargs):
+    """Update the status/details of an existing job."""
+    with _lock:
+        if job_id in _jobs:
+            _jobs[job_id].update(kwargs)
+
+
+def get_job(job_id):
+    """Retrieve a copy of a job."""
+    with _lock:
+        job = _jobs.get(job_id)
+
+        if job is None:
+            return None
+
+        return dict(job)
